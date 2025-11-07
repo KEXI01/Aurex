4bc7f81df0d06edba7e8b2c5b611b66e333fd2bf
Prakhar Shukla <SmokenX1@gmail.com>
2025-11-07 13:59:37 +0530

    Update logger.py

--- Opus/utils/logger.py
+++ Opus/utils/logger.py
@@ -2,32 +2,42 @@ from pyrogram.enums import ParseMode
 
 from Opus import app
 from Opus.utils.database import is_on_off
+from Opus.misc import SUDOERS
 from config import LOGGER_ID
 
 
 async def play_logs(message, streamtype):
-    if await is_on_off(2):
-        logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
-
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
-
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
-
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
-        if message.chat.id != LOGGER_ID:
-            try:
-                await app.send_message(
-                    chat_id=LOGGER_ID,
-                    text=logger_text,
-                    parse_mode=ParseMode.HTML,
-                    disable_web_page_preview=True,
-                )
-            except:
-                pass
+    if not await is_on_off(2):
         return
+
+    if message.from_user.id in SUDOERS:
+        return
+
+    log_text = f"""
+<b>{app.mention} •Current Playback logs</b>
+────────────────────
+
+<b>Chat:</b> {message.chat.title or 'Private Chat'}
+<b>Chat_ID:</b> <code>{message.chat.id}</code>
+<b>Username:</b> @{message.chat.username or 'U/A'}
+
+<b>User:</b> {message.from_user.mention}
+<b>User_ID:</b> <code>{message.from_user.id}</code>
+<b>Handle:</b> @{message.from_user.username or 'U/A'}
+
+<b>Track Query:</b> {message.text.split(None, 1)[1]}
+<b>Stream :</b> {streamtype}
+────────────────────
+"""
+
+    if message.chat.id != LOGGER_ID:
+        try:
+            await app.send_message(
+                chat_id=LOGGER_ID,
+                text=log_text,
+                parse_mode=ParseMode.HTML,
+                disable_web_page_preview=True,
+            )
+        except:
+            pass
+            
