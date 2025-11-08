b84413a784ed6c5e941f5b4d72026113da907a4c
Prakhar Shukla <SmokenX1@gmail.com>
2025-11-08 09:19:42 +0530

    Update ndatabase.py

--- Opus/utils/ndatabase.py
+++ Opus/utils/ndatabase.py
@@ -4,6 +4,8 @@ from typing import Dict, List, Union, Optional
 
 from Opus import userbot
 from Opus.core.mongo import mongodb
+from Opus.core.userbot import assistants
+
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
@@ -69,8 +71,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from Opus.core.userbot import assistants
-
+    
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
     await assdb.update_one(
@@ -110,8 +111,6 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from Opus.core.userbot import assistants
-
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
     await assdb.update_one(
@@ -123,8 +122,6 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from Opus.core.userbot import assistants
-
     assistant = assistantdict.get(chat_id)
     if not assistant:
         dbassistant = await assdb.find_one({"chat_id": chat_id})
