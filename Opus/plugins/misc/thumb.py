375178e65e21be02b7ed039fa241dbf31b7bb8c6
Prakhar Shukla <SmokenX1@gmail.com>
2025-11-27 03:50:22 +0530

    Update thumb.py

--- Opus/plugins/misc/thumb.py
+++ Opus/plugins/misc/thumb.py
@@ -8,15 +8,15 @@ async def thumb_toggle(_, message):
     if len(message.command) < 2:
         current = await get_thumb_setting(chat_id)
         if current:
-            await message.reply_text("sᴏɴɢ ᴛʜᴜᴍʙɴᴀɪʟs : Oɴ ✅\nᴛʜᴜᴍʙɴᴀɪʟ Wɪʟʟ ʙᴇ ᴠɪsɪʙʟᴇ ᴀʟᴏɴɢ Wɪᴛʜ sᴏɴɢ ɪɴғᴏ")
+            await message.reply_text("ᴛʜᴜᴍʙɴᴀɪʟ : Oɴ ✅\nᴛʜᴜᴍʙɴᴀɪʟ wɪʟʟ ʙᴇ ᴠɪsɪʙʟᴇ ᴀʟᴏɴɢ wɪᴛʜ sᴏɴɢ ɪɴғᴏ")
         else:
-            await message.reply_text("Tʜᴜᴍʙɴᴀɪʟs : ᴏғғ 🚫\nᴏɴʟʏ sᴏɴɢ ɪɴғᴏ Wʟʟ ʙᴇ ᴠɪsɪʙʟᴇ ɴᴏW ɪɴ ᴛʜɪs ᴄʜᴀᴛ.")
+            await message.reply_text("Tʜᴜᴍʙɴᴀɪʟ : ᴏғғ 🚫\nᴏɴʟʏ sᴏɴɢ ɪɴғᴏ Wʟʟ ʙᴇ ᴠɪsɪʙʟᴇ ɴᴏW ɪɴ ᴛʜɪs ᴄʜᴀᴛ.")
         return
 
     arg = message.command[1].lower()
     if arg == "on":
         await set_thumb_setting(chat_id, True)
-        await message.reply_text("sᴏɴɢ Tʜᴜᴍʙɴᴀɪʟs ᴛᴜʀɴᴇᴅ ᴏɴ 🧩 ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ.")
+        await message.reply_text("sᴏɴɢ Tʜᴜᴍʙɴᴀɪʟ ᴛᴜʀɴᴇᴅ ᴏɴ 🧩 ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ.")
     elif arg == "off":
         await set_thumb_setting(chat_id, False)
         await message.reply_text("ᴛʜᴜᴍʙɴᴀɪʟs ᴛᴜʀɴᴇᴅ ᴏғғ 🔖 ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ.")
