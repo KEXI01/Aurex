ef7d2cfdf7f5e05d7607b09353cfe7175f2f47ec
Prakhar Shukla <SmokenX1@gmail.com>
2025-11-27 03:46:12 +0530

    refractions/.

--- Opus/plugins/misc/thumb.py
+++ Opus/plugins/misc/thumb.py
@@ -8,15 +8,15 @@ async def thumb_toggle(_, message):
     if len(message.command) < 2:
         current = await get_thumb_setting(chat_id)
         if current:
-            await message.reply_text("Thumb: ON 🟢\nThumbnails will be sent along with songs.")
+            await message.reply_text("sᴏɴɢ ᴛʜᴜᴍʙɴᴀɪʟs : Oɴ ✅\nᴛʜᴜᴍʙɴᴀɪʟ Wɪʟʟ ʙᴇ ᴠɪsɪʙʟᴇ ᴀʟᴏɴɢ Wɪᴛʜ sᴏɴɢ ɪɴғᴏ")
         else:
-            await message.reply_text("Thumb: OFF🔴\nOnly song info caption with controls will be sent.")
+            await message.reply_text("Tʜᴜᴍʙɴᴀɪʟs : ᴏғғ 🚫\nᴏɴʟʏ sᴏɴɢ ɪɴғᴏ Wʟʟ ʙᴇ ᴠɪsɪʙʟᴇ ɴᴏW ɪɴ ᴛʜɪs ᴄʜᴀᴛ.")
         return
 
     arg = message.command[1].lower()
     if arg == "on":
         await set_thumb_setting(chat_id, True)
-        await message.reply_text("Thumb: ON🟩\nThumbnails will now appear with songs.")
+        await message.reply_text("sᴏɴɢ Tʜᴜᴍʙɴᴀɪʟs ᴛᴜʀɴᴇᴅ ᴏɴ 🧩 ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ.")
     elif arg == "off":
         await set_thumb_setting(chat_id, False)
-        await message.reply_text("Thumb: OFF🟥\nSongs will now send only info captions with playback control buttons.")
+        await message.reply_text("ᴛʜᴜᴍʙɴᴀɪʟs ᴛᴜʀɴᴇᴅ ᴏғғ 🔖 ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ.")
