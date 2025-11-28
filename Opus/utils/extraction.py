89744eea6ead2482e6527ceac45d7156894629c4
Prakhar Shukla <SmokenX1@gmail.com>
2025-11-28 07:04:35 +0530

    fix entity indexes

--- Opus/utils/extraction.py
+++ Opus/utils/extraction.py
@@ -1,17 +1,28 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
-
 from Opus import app
-
 
 async def extract_user(m: Message) -> User:
     if m.reply_to_message:
         return m.reply_to_message.from_user
-    msg_entities = m.entities[1] if m.text.startswith("/") else m.entities[0]
-    return await app.get_users(
-        msg_entities.user.id
-        if msg_entities.type == MessageEntityType.TEXT_MENTION
-        else int(m.command[1])
-        if m.command[1].isdecimal()
-        else m.command[1]
-    )
+
+    text = m.text or ""
+    entities = m.entities or []
+
+    if text.startswith("/") and len(entities) > 1:
+        ent = entities[1]
+    elif len(entities) > 0:
+        ent = entities[0]
+    else:
+        ent = None
+
+    if ent and ent.type == MessageEntityType.TEXT_MENTION:
+        return await app.get_users(ent.user.id)
+
+    if m.command and len(m.command) > 1:
+        arg = m.command[1]
+        if arg.isdecimal():
+            return await app.get_users(int(arg))
+        return await app.get_users(arg)
+
+    return None
