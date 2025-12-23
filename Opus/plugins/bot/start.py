380ecfe26664e45cfe6bc554911d93e7c0dda811
K Σ X ! <167321927+KEXI01@users.noreply.github.com>
2025-12-23 22:29:51 +0530

    Update start.py

--- Opus/plugins/bot/start.py
+++ Opus/plugins/bot/start.py
@@ -22,20 +22,12 @@ from config import BANNED_USERS
 from strings import get_string
 
 
-def _user_link(user):
-    if user.username:
-        return f"<a href='https://t.me/{user.username}'>ᴀ ᴜsᴇʀ</a>"
-    return f"<a href='tg://user?id={user.id}'>>ᴀ ᴜsᴇʀ</a>"
-
-
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
-
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
-
         if name[0:4] == "help":
             keyboard = help_pannel(_)
             return await message.reply_photo(
@@ -43,50 +35,31 @@ async def start_pm(client, message: Message, _):
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
-
         if name[0:3] == "sud":
             await sudoers_list(client=client, message=message, _=_)
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"<blockquote><b>» {_user_link(message.from_user)} sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ sᴜᴅᴏʟɪsᴛ</b>\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code></blockquote>",
+                    text=f"<blockquote><b>» <a href='https://t.me/{message.from_user.username}'>ᴜsᴇʀ</a> ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ sᴜᴅᴏʟɪsᴛ</b>\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code></blockquote>",
                     disable_web_page_preview=True
                 )
             return
-
         if name[0:3] == "inf":
             m = await message.reply_text("🔎")
-
-            query = str(name).replace("info_", "", 1)
+            query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
-
             results = VideosSearch(query, limit=1)
-            data = results.result()
-
-            if not data or not data.get("result"):
-                await m.delete()
-                return await message.reply_text(_["general_2"].format("No results found"))
-
-            result = data["result"][0]
-
-            title = result["title"]
-            duration = result["duration"]
-            views = result["viewCount"]["short"]
-
-            channel_data = result.get("channel", {})
-            channellink = channel_data.get("link")
-            if not channellink:
-                cid = channel_data.get("id")
-                channellink = f"https://www.youtube.com/channel/{cid}" if cid else "https://www.youtube.com"
-
-            channel = channel_data.get("name")
-            link = result["link"]
-            published = result["publishedTime"]
-
+            for result in (await results.next())["result"]:
+                title = result["title"]
+                duration = result["duration"]
+                views = result["viewCount"]["short"]
+                channellink = result["channel"]["link"]
+                channel = result["channel"]["name"]
+                link = result["link"]
+                published = result["publishedTime"]
             searched_text = _["start_6"].format(
                 title, duration, views, published, channellink, channel, app.mention
             )
-
             key = InlineKeyboardMarkup(
                 [
                     [
@@ -95,32 +68,31 @@ async def start_pm(client, message: Message, _):
                     ],
                 ]
             )
-
             await m.delete()
-            await message.reply(text=searched_text, reply_markup=key)
-
+            await message.reply(
+                text=searched_text,
+                reply_markup=key,
+            )
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"<blockquote><b>» {_user_link(message.from_user)} sᴛᴀʀᴛᴇᴅ ᴛʜᴇ sᴛᴏʀᴍ ᴛᴏ ᴄʜᴇᴄᴋ sᴏɴɢ ᴅᴇᴛᴀɪʟs</b>\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code></blockquote>",
+                    text=f"<blockquote><b>» <a href='https://t.me/{message.from_user.username}'>ᴜsᴇʀ</a> ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code></blockquote>",
                     disable_web_page_preview=False
                 )
-            return
-
-    out = private_panel(_)
-    await message.reply(
-        text='<blockquote><b>Hᴇʏ, I’ᴍ 𝑺ᴛᴏʀᴍ, 🧸</b></blockquote>\n<blockquote><b>ʏᴏᴜʀ ᴘᴏᴡᴇʀꜰᴜʟ ᴍᴜꜱɪᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ. ʙᴜɪʟᴛ ᴛᴏ ʙʀɪɴɢ ʜɪ-ʀᴇs ꜱᴏᴜɴᴅ, ꜱᴍᴏᴏᴛʜ ᴄᴏɴᴛʀᴏʟꜱ, ᴀɴ ᴇʟɪᴛᴇ ʟɪꜱᴛᴇɴɪɴɢ ᴇxᴘᴇʀɪᴇɴᴄᴇ ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘꜱ & ᴄʜᴀɴɴᴇʟs.</b></blockquote>\n<b><blockquote><a href="https://files.catbox.moe/n2l0wd.jpg">✨</a> ᴡʜᴀᴛ ɪ ᴅᴏ:\n• ᴘʟᴀʏs ʜɪɢʜ-𝑄ᴜᴀʟɪᴛʏ ᴍᴜꜱɪᴄ\n• ᴄᴏɴᴛʀᴏʟꜱ ➕ ᴄʟᴇᴀɴ ᴘᴇʀꜰᴏʀᴍᴀɴᴄᴇ\n• ᴄᴏᴏʟ ғᴇᴀᴛᴜʀᴇꜱ ғᴏʀ ʏᴏᴜʀ ᴄʜɪᴛʏ ᴄʜᴀᴛ ᴠɪʙᴇꜱ</blockquote></b>\n<blockquote><b>📚 ɴᴇᴇᴅ ʜᴇʟᴘ ?\nᴛᴀᴘ ʜᴇʟᴘ ᴛᴏ ꜱᴇᴇ ᴀʟʟ ᴍʏ ᴄᴏᴍᴍᴀɴᴅꜱ.</b></blockquote>',
-        reply_markup=InlineKeyboardMarkup(out),
-    )
-
-    if await is_on_off(2):
-        if message.from_user.id in SUDOERS:
-            return
-        return await app.send_message(
-            chat_id=config.LOGGER_ID,
-            text=f"» {_user_link(message.from_user)} sᴛᴀʀᴛᴇᴅ ᴛʜᴇ sᴛᴏʀᴍ.\nᴜsᴇʀ ɪᴅ : <code>{message.from_user.id}</code>",
-            disable_web_page_preview=True
+    else:
+        out = private_panel(_)         
+        await message.reply(
+            text='<blockquote><b>Hᴇʏ, I’ᴍ ꜱᴛᴏʀᴍ, 🧸</b></blockquote>\n<blockquote><b>ʏᴏᴜʀ ᴘᴏᴡᴇʀꜰᴜʟ ᴍᴜꜱɪᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ. ʙᴜɪʟᴛ ᴛᴏ ʙʀɪɴɢ ᴘᴏᴡᴇʀꜰᴜʟ ꜱᴏᴜɴᴅ, ꜱᴍᴏᴏᴛʜ ᴄᴏɴᴛʀᴏʟꜱ, ᴀɴᴅ ᴀɴ ᴇʟɪᴛᴇ ʟɪꜱᴛᴇɴɪɴɢ ᴇxᴘᴇʀɪᴇɴᴄᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘꜱ.</b></blockquote>\n<b><blockquote><a href="https://files.catbox.moe/n2l0wd.jpg">✨</a> ᴡʜᴀᴛ ɪ ᴅᴏ:\n• ᴘʟᴀʏ ʜɪɢʜ-Qᴜᴀʟɪᴛʏ ᴍᴜꜱɪᴄ\n• ꜰᴀꜱᴛ ᴄᴏɴᴛʀᴏʟꜱ & ᴄʟᴇᴀɴ ᴘᴇʀꜰᴏʀᴍᴀɴᴄᴇ\n• ᴄᴏᴏʟ ꜰᴇᴀᴛᴜʀᴇꜱ ꜰᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴠɪʙᴇꜱ</blockquote></b>\n<blockquote><b>📚 ɴᴇᴇᴅ ʜᴇʟᴘ?\nᴛᴀᴘ ʜᴇʟᴘ ᴛᴏ ꜱᴇᴇ ᴀʟʟ ᴍʏ ᴄᴏᴍᴍᴀɴᴅꜱ.</b></blockquote>',
+            reply_markup=InlineKeyboardMarkup(out),
         )
+        if await is_on_off(2):
+            if message.from_user.id in SUDOERS:
+                return
+            return await app.send_message(
+                chat_id=config.LOGGER_ID,
+                text=f"» <a href='https://t.me/{message.from_user.username}'>user</a> just started the bot.\nuser id : <code>{message.from_user.id}</code>\n<a href='tg://user?id={message.from_user.id}'>profile link</a>",
+                disable_web_page_preview=True
+            )
 
 
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
@@ -141,18 +113,15 @@ async def welcome(client, message: Message):
         try:
             language = await get_lang(message.chat.id)
             _ = get_string(language)
-
             if await is_banned_user(member.id):
                 try:
                     await message.chat.ban_member(member.id)
                 except:
                     pass
-
             if member.id == app.id:
                 if message.chat.type != ChatType.SUPERGROUP:
                     await message.reply_text(_["start_4"])
                     return await app.leave_chat(message.chat.id)
-
                 if message.chat.id in await blacklisted_chats():
                     await message.reply_text(
                         _["start_5"].format(
@@ -176,6 +145,5 @@ async def welcome(client, message: Message):
                 )
                 await add_served_chat(message.chat.id)
                 await message.stop_propagation()
-
         except Exception as ex:
             print(ex)
